0333b8bd443e2aad0f3acb6b3ce176e87e679264
SunilKrGupta3 <sunilsagargupta7033@gmail.com>
2024-09-16 22:44:46 +0530

    add find factorial

--- PR/src/com/springgit/in/PR1.java
+++ PR/src/com/springgit/in/PR1.java
@@ -1,9 +1,27 @@
 package com.springgit.in;
 
+import java.util.Scanner;
+
 public class PR1 {
 
-	public static void main(String[] args) {
+	 public static long factorial(int num){
+		    int fact=1;
+		    int i=1;
+		    while(i<=num) {
+		        fact = fact * i;
+		        i++;
+		    }
+		        return fact;
+		    }
+
+		    public static void main(String[] args) {
+		        Scanner sc=new Scanner(System.in);
+		        System.out.print("Enter N number: ");
+		        int num=sc.nextInt();
+		           long fact=factorial(num);
+		        System.out.println(fact);
 		System.out.println("Hello");
+		
 
 	}
 
